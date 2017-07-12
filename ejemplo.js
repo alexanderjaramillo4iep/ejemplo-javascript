176aeb3366c99fabc8094e577a0194ed8c2ea501
alexun <alexunjm@gmail.com>
2017-07-12 10:25:28 -0500

    cambio en mensaje de aceptar en confirm

--- ejemplo.js
+++ ejemplo.js
@@ -1,7 +1,7 @@
 
 if(confirm("Estas contento en clase?")) {
 	// lo que se hace si se cumple la condición
-	alert("me caes bien");
+	alert("en realidad todos los del grupo me caen bien");
 } else {
 	// lo que se hace si NO se cumple la condición
 	alert("dígale al profesor como hace la clase más divertida");
