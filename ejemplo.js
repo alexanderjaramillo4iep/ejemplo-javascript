345278277f37accd45815a5bf29859dd8100caff
alexun <alexunjm@gmail.com>
2017-07-12 09:44:23 -0500

    ejemplo de javascript con alert y confirm

---
+++ ejemplo.js
@@ -0,0 +1,8 @@
+
+if(confirm("Estas contento en clase?")) {
+	// lo que se hace si se cumple la condición
+	alert("me caes bien");
+} else {
+	// lo que se hace si NO se cumple la condición
+	alert("mejor quedese durmiendo en la casa");
+}
